c5417354019b36e0ebb16ebb69f10be68104f1c3
Szymon <majewskis@gmail.com>
2016-11-29 01:16:33 -0800

    trivial implementation on revealing tiles

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -29,6 +29,9 @@ public class Gameplay : MonoBehaviour
 
     private void HandleBoardTileTapped(int x, int y)
     {
+        m_board.Image.SetPixel(x, y, m_image.Texture.GetPixel(x, y));
+        m_board.Image.Apply();
+
         Debug.LogFormat("tap: {0}, {1}", x, y);
     }
 }
