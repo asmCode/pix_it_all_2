5f463a3c04a0abdc01d08b9499e97dafaebb6c7e
Szymon <majewskis@gmail.com>
2016-12-04 13:37:35 -0800

    fixed palette placement on different screens

--- Assets/Scripts/Palette.cs
+++ Assets/Scripts/Palette.cs
@@ -5,14 +5,11 @@ public class Palette : MonoBehaviour
 {
     public PaletteStrip m_paletteStripPrefab;
     public RectTransform m_stripContainer;
+    public RectTransform m_rowPivot;
 
     public void Awake()
     {
         int rowCount = 9;
-
-        float stepX = 50.0f;
-        float initXPos = stepX * (rowCount - 1) / 2.0f;
-        float xPos = initXPos;
 
         float stepAngle = 3.2f;
         float initAnglePos = stepAngle * (rowCount - 1) / 2.0f;
@@ -23,24 +20,10 @@ public class Palette : MonoBehaviour
             var paletteStrip = Instantiate(m_paletteStripPrefab);
             var rectTr = paletteStrip.gameObject.GetComponent<RectTransform>();
             rectTr.parent = m_stripContainer.transform;
-            //rectTr.localPosition = new Vector3(xPos, -Mathf.Abs(xPos * 0.2f), 0);
             rectTr.localPosition = Vector3.zero;
-            //rectTr.localRotation = Quaternion.AngleAxis(angle, Vector3.back);
-            rectTr.RotateAround(new Vector3(rectTr.parent.position.x, rectTr.parent.position.y - 1000, 0), Vector3.back, angle);
-            xPos -= stepX;
+            rectTr.localScale = Vector3.one;
+            rectTr.RotateAround(m_rowPivot.position, Vector3.back, angle);
             angle -= stepAngle;
         }
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
