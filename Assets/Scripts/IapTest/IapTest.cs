c10faff107b586c82937df15b7bdbdfbf3280439
Szymon <majewskis@gmail.com>
2017-01-30 22:24:56 -0800

    iap test

---
+++ Assets/Scripts/IapTest/IapTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IapTest : MonoBehaviour
+{
+
+    public Purchaser purchaser;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BuyClicked()
+    {
+        // purchaser.BuyNonConsumable();
+        purchaser.BuyConsumable();
+    }
+}
