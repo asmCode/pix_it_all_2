e896ed335a711edbaa1695402148dde777c4a162
Szymon <majewskis@gmail.com>
2017-04-10 23:56:29 -0700

    tweaks

--- Assets/Scripts/TapGestureDetector.cs
+++ Assets/Scripts/TapGestureDetector.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TapGestureDetector : MonoBehaviour
 {
-    private const float m_tapInchThreshold = 0.1f;
+    private const float m_tapInchThreshold = 0.08f;
 
     public event System.Action<Vector2> Tapped;
 
