9202cf02d68230a9f3d73cf206ab64232abbd024
Majak <majakthecoder@gmail.com>
2017-05-21 01:02:09 -0700

    changed iap ids

--- Assets/Scripts/IapStore/IapIds.cs
+++ Assets/Scripts/IapStore/IapIds.cs
@@ -6,9 +6,9 @@
         //"pixitall.bundle.price1.02",
         //"pixitall.bundle.price2.03",
 
-        "pixitall.bundle.price1.01",
-        "test.pixitall.01",
-        "test.pixitall.02",
-        "test.pixitall.03",
+        "pixitallfree.bundle.price1.01",
+        "test.pixitallfree.01",
+        "test.pixitallfree.02",
+        "test.pixitallfree.03",
     };
 }
