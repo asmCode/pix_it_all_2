fa663a302f521ac56c7cd6533bcd987f3c3a94ce
Szymon <majewskis@gmail.com>
2017-06-06 23:16:41 -0700

    Reskinned RateMe, fixed problems with the layouting

---
+++ Assets/Scripts/UpdateLayout.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateLayout : MonoBehaviour
+{
+    private RectTransform m_rectTransform;
+
+    private void Awake()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        LayoutRebuilder.MarkLayoutForRebuild(m_rectTransform);
+    }
+}
